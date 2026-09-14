00b09681c5b8f1d86b7884154f075200fe46323f
CedricLec05 <cedric.leclerc123@gmail.com>
2026-09-14 13:45:39 -0400

    9/14

--- Assets/Scripts/Balle.cs
+++ Assets/Scripts/Balle.cs
@@ -12,16 +12,19 @@ public class Balle : MonoBehaviour
 
 
 
-    // [Header("Paramètres de tir")]
-    private float force;
+    [Header("Paramètres de tir")]
+    Rigidbody rbBalle;
 
 
+    [Header("Gauge de force")]
+    [SerializeField] float forceTir;
+    [SerializeField] float accumulateurForce = 0.5f;
 
-    // [Header("Gauge de force")]
+    [SerializeField] Slider jaugeForce;
 
 
-    // [Header("Input Actions")]
-    [SerializeField] private InputAction espace;
+    [Header("Input Actions")]
+    [SerializeField] private InputAction tirAction;
 
 
     // [Header("Composant")]
@@ -29,13 +32,29 @@ public class Balle : MonoBehaviour
 
     void Start()
     {
-
+        rbBalle = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-
-
+        if (tirAction.WasPressedThisFrame())
+        {
+            forceTir = 0;
+            jaugeForce.value = forceTir;
+        }
+        if (tirAction.IsPressed())
+        {
+            forceTir+= accumulateurForce;
+            forceTir = Mathf.Clamp(forceTir,jaugeForce.minValue,jaugeForce.maxValue);
+            jaugeForce.value = forceTir;
+        }
+        if (tirAction.WasReleasedThisFrame())
+        {
+            //Envoyer balle
+            rbBalle.AddForce(Vector3.forward*forceTir, ForceMode.Impulse);
+            forceTir = 0;
+            jaugeForce.value = forceTir;
+        }
     }
 
     void OnCollisionEnter(Collision collision)
@@ -72,11 +91,11 @@ public class Balle : MonoBehaviour
     // Gestion des inputs actions
     void OnEnable()
     {
-        espace.Enable();
+        tirAction.Enable();
     }
 
     void OnDisable()
     {
-        espace.Disable();
+        tirAction.Disable();
     }
 }
