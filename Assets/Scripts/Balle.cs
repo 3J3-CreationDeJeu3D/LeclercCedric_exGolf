cad24d14953a4f3260519a0c1933d9212b252dc4
CedricLec05 <cedric.leclerc123@gmail.com>
2026-09-21 13:47:47 -0400

    commit 21/09
    bomboclat

--- Assets/Scripts/Balle.cs
+++ Assets/Scripts/Balle.cs
@@ -4,39 +4,58 @@ using UnityEngine.UI;
 using TMPro;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using System.Runtime.CompilerServices;
 
 public class Balle : MonoBehaviour
 {
 
-    // [Header("État de jeu")]
-
+    [Header("État de jeu")]
+    Vector3 positionBalle;
+    private int nbCoups;
 
 
     [Header("Paramètres de tir")]
     Rigidbody rbBalle;
+    [SerializeField] float angleTir;
 
 
     [Header("Gauge de force")]
     [SerializeField] float forceTir;
-    [SerializeField] float accumulateurForce = 0.5f;
+    [SerializeField] float accumulateurForce = 2f;
 
     [SerializeField] Slider jaugeForce;
 
 
     [Header("Input Actions")]
     [SerializeField] private InputAction tirAction;
+    [SerializeField] private InputAction angleAction;
 
 
-    // [Header("Composant")]
-
+    [Header("Composant")]
+    LineRenderer lineRendererBalle;
+    [SerializeField] TMP_Text coupsTexte;
+    AudioSource audioSourceBalle;
+    [SerializeField] AudioClip sonErreur;
+    [SerializeField] AudioClip sonFin;
 
     void Start()
     {
         rbBalle = GetComponent<Rigidbody>();
+        lineRendererBalle = GetComponent<LineRenderer>();
+        audioSourceBalle = GetComponent<AudioSource>();
+        nbCoups = 0;
     }
 
     void Update()
     {
+        angleTir += angleAction.ReadValue<float>();
+        Vector3 direction = Quaternion.Euler(0,angleTir, 0) * Vector3.forward;
+        lineRendererBalle.SetPosition(0, transform.position);
+        lineRendererBalle.SetPosition(1, transform.position + direction);
+
         if (tirAction.WasPressedThisFrame())
         {
             forceTir = 0;
@@ -51,20 +70,37 @@ public class Balle : MonoBehaviour
         if (tirAction.WasReleasedThisFrame())
         {
             //Envoyer balle
-            rbBalle.AddForce(Vector3.forward*forceTir, ForceMode.Impulse);
+            positionBalle = transform.position;
+            rbBalle.AddForce(direction*forceTir*Time.deltaTime, ForceMode.Impulse);
             forceTir = 0;
             jaugeForce.value = forceTir;
+            nbCoups++;
         }
+
+        coupsTexte.text = $"{nbCoups} coup(s)";
     }
 
     void OnCollisionEnter(Collision collision)
     {
-
+        if(collision.gameObject.tag == "horsParcours")
+        {
+            audioSourceBalle.PlayOneShot(sonErreur);
+            rbBalle.linearVelocity = Vector3.zero;
+            rbBalle.angularVelocity = Vector3.zero;
+            transform.position = positionBalle;
+        }
     }
 
     void OnTriggerEnter(Collider collision)
     {
-
+        if(collision.gameObject.tag == "trou")
+        {
+            audioSourceBalle.PlayOneShot(sonFin);
+            rbBalle.linearVelocity = Vector3.zero;
+            rbBalle.angularVelocity = Vector3.zero;
+            rbBalle.useGravity = false;
+            transform.position = collision.transform.position;
+        }
     }
 
     // ===================
@@ -92,10 +128,12 @@ public class Balle : MonoBehaviour
     void OnEnable()
     {
         tirAction.Enable();
+        angleAction.Enable();
     }
 
     void OnDisable()
     {
         tirAction.Disable();
+        angleAction.Disable();
     }
 }
